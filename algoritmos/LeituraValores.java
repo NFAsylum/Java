03f8973dc0a6b23d10c21a7e2fab6d6149602a23
NFAsylum <119449613+NFAsylum@users.noreply.github.com>
2024-03-09 09:00:04 -0300

    Add files via upload

--- algoritmos/LeituraValores.java
+++ algoritmos/LeituraValores.java
@@ -3,10 +3,13 @@ import javax.swing.JOptionPane;
 public class LeituraValores {
     public static void main(String[] args)
     {
-        String input = JOptionPane.showInputDialog(null, "Digite um inteiro");
-        
-        int value = Integer.parseInt(input) * 2;
+        //Receving first value and using parse in another line
+        String input = JOptionPane.showInputDialog("Digite um inteiro:");
+        int value1 = Integer.parseInt(input);
 
-        JOptionPane.showMessageDialog(null, "Seu valor x2 vale: " + value);
+        //Receiving second value and using parse
+        int value2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro inteiro:"));
+
+        JOptionPane.showMessageDialog(null, "Seus valores somados valem: " + (value1 + value2));
     }
 }
